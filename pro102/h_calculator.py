850a4dfa0100097fd8784f5fc4a1c2c4287bcf87
PampleMousse <77309642+Archith007@users.noreply.github.com>
2022-01-02 19:59:57 -0500

    Add files via upload

---
+++ pro102/h_calculator.py
@@ -0,0 +1,10 @@
+print("this project will calculate your speed in km per min")
+
+dis = float(input("enter your distance travelled (in km)"))
+time = float(input("enter your time (in mins)"))
+
+speed = dis/time
+print("your speed is",speed,"km/min")
+
+
+
